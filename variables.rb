568e539c2f268ff0e1fc6192e5b723d33fd84b82
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-04 10:23:23 +0000

    Variables comments added

--- variables.rb
+++ variables.rb
@@ -1,8 +1,8 @@
-cars = 100
+cars = 100 #100 cars available
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
-cars_not_driven = cars - drivers
+cars_not_driven = cars - drivers #this subtracts the number of drivers from the number of cars available
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
