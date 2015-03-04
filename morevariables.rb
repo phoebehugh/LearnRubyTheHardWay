e31a8b0ba7b1cd13db43071a88b8f89f1218d725
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-04 10:42:38 +0000

    More variables

---
+++ morevariables.rb
@@ -0,0 +1,17 @@
+my_name = gets.chomp
+my_age = 25 #this is true in 2015!
+my_height = 62 #inches
+my_weight = 54 #kilograms
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Blonde'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches small."
+puts "She's #{my_weight} kilograms heavy."
+puts "Actually, that is pretty light!"
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} as long as she hasn't gulped down too much tea."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
