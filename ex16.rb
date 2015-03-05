ed637dd30c62f41d9e1f45a148282a89db2ffdfb
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-05 09:34:14 +0000

    Updated so that target.write is on one line

--- ex16.rb
+++ ex16.rb
@@ -23,12 +23,9 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+
+#One line for all of the target.write lines
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
 
 puts "And finally, we close it."
 target.close
